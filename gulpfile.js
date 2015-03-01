59cdc7e2df1cfc99b5cbd41fa793477ac28adc28
Kasper Isager <kasperisager@gmail.com>
2015-03-02 00:53:52 +0100

    Remove copyright headers.

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,6 @@ var gulp = require("gulp")
   , $ = require("gulp-load-plugins")()
   , del = require("del")
 
-var copyright = [
-  "/*!"
-, " * Copyright (c) 2015 Modem."
-, " *"
-, " * Permission is hereby granted, free of charge, to any person obtaining a copy"
-, " * of this software and associated documentation files (the \"Software\"), to"
-, " * deal in the Software without restriction, including without limitation the"
-, " * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or"
-, " * sell copies of the Software, and to permit persons to whom the Software is"
-, " * furnished to do so, subject to the following conditions:"
-, " *"
-, " * The above copyright notice and this permission notice shall be included in"
-, " * all copies or substantial portions of the Software."
-, " *"
-, " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR"
-, " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,"
-, " * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE"
-, " * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER"
-, " * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING"
-, " * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER"
-, " * DEALINGS IN THE SOFTWARE."
-, " */"
-]
-
 gulp.task("css", ["css:clean"], function () {
   return gulp.src("css/*.css")
     .pipe($.plumber())
@@ -35,7 +11,6 @@ gulp.task("css", ["css:clean"], function () {
     .pipe($.minifyCss({
       keepSpecialComments: false
     }))
-    .pipe($.header(copyright.join("\n") + "\n"))
     .pipe(gulp.dest("dist/css"))
     .pipe($.livereload())
     .pipe($.size({
@@ -59,7 +34,6 @@ gulp.task("js", ["js:clean"], function () {
     .pipe($.uglify({
       preserveComments: false
     }))
-    .pipe($.header(copyright.join("\n") + "\n"))
     .pipe(gulp.dest("dist/js"))
     .pipe($.livereload())
     .pipe($.size({
