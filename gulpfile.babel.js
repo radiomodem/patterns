9e6b5ad066750f9f16facbb03d4dc64770f98ea3
Kasper Isager <kasperisager@gmail.com>
2015-11-14 16:10:32 +0100

    Update gulpfile and deps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,127 +2,114 @@ const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 const del = require('del');
 
-gulp.task('css', ['css:clean'], () => {
-  return gulp.src('css/*.css')
-    .pipe($.plumber())
-    .pipe($.cssnext({
-      sourcemap: true
-    }))
-    .pipe($.csslint('css/.csslintrc'))
-    .pipe($.csslint.reporter())
-    .pipe($.csslint.reporter('fail'))
-    .pipe($.sourcemaps.init({
-      loadMaps: true
-    }))
-    .pipe($.minifyCss({
-      keepSpecialComments: false
-    }))
-    .pipe($.sourcemaps.write('.', {
-      sourceRoot: '/src/css'
-    }))
-    .pipe(gulp.dest('dist/css'))
-    .pipe($.livereload())
-    .pipe($.size({
-      title: 'css',
-      showFiles: true
-    }));
-});
+gulp.task('css', ['css:clean'], () => gulp.src('css/*.css')
+  .pipe($.plumber())
+  .pipe($.cssnext({
+    sourcemap: true
+  }))
+  .pipe($.csslint('css/.csslintrc'))
+  .pipe($.csslint.reporter())
+  .pipe($.csslint.reporter('fail'))
+  .pipe($.sourcemaps.init({
+    loadMaps: true
+  }))
+  .pipe($.minifyCss({
+    keepSpecialComments: false
+  }))
+  .pipe($.sourcemaps.write('.', {
+    sourceRoot: '/src/css'
+  }))
+  .pipe(gulp.dest('dist/css'))
+  .pipe($.livereload())
+  .pipe($.size({
+    title: 'css',
+    showFiles: true
+  }))
+);
 
-gulp.task('css:clean', done => {
-  del(['dist/css'], done);
-});
+gulp.task('css:clean', () => del(['dist/css']));
 
-gulp.task('css:stats', ['css'], () => {
-  return gulp.src('dist/css/*.css')
-    .pipe($.plumber())
-    .pipe($.parker());
-});
+gulp.task('css:stats', ['css'], () => gulp.src('dist/css/*.css')
+  .pipe($.plumber())
+  .pipe($.parker())
+);
 
-gulp.task('js', ['js:clean'], () => {
-  return gulp.src('js/**/*.js')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.babel({
-      modules: 'umd',
-      moduleIds: true
-    }))
-    .pipe($.concat('modem.js'))
-    .pipe($.uglify({
-      preserveComments: false
-    }))
-    .pipe($.sourcemaps.write('.', {
-      sourceRoot: '/src/js'
-    }))
-    .pipe(gulp.dest('dist/js'))
-    .pipe($.livereload())
-    .pipe($.size({
-      title: 'js',
-      showFiles: true
-    }));
-});
+gulp.task('js', ['js:clean'], () => gulp.src('js/**/*.js')
+  .pipe($.plumber())
+  .pipe($.sourcemaps.init())
+  .pipe($.babel({
+    modules: 'umd',
+    moduleIds: true
+  }))
+  .pipe($.concat('modem.js'))
+  .pipe($.uglify({
+    preserveComments: false
+  }))
+  .pipe($.sourcemaps.write('.', {
+    sourceRoot: '/src/js'
+  }))
+  .pipe(gulp.dest('dist/js'))
+  .pipe($.livereload())
+  .pipe($.size({
+    title: 'js',
+    showFiles: true
+  }))
+);
 
-gulp.task('js:clean', done => {
-  del(['dist/js'], done);
-});
+gulp.task('js:clean', () => del(['dist/js']));
 
-gulp.task('img', ['img:clean'], () => {
-  return gulp.src('img/**')
-    .pipe($.plumber())
-    .pipe($.changed('dist/img'))
-    .pipe($.imagemin({
-      svgoPlugins: [
-        {removeTitle: true},
-        {removeDesc: true}
-      ]
-    }))
-    .pipe(gulp.dest('dist/img'))
-    .pipe($.size({
-      title: 'img',
-      showFiles: true
-    }));
-});
+gulp.task('img', ['img:clean'], () => gulp.src('img/**')
+  .pipe($.plumber())
+  .pipe($.changed('dist/img'))
+  .pipe($.imagemin({
+    svgoPlugins: [
+      {removeTitle: true},
+      {removeDesc: true}
+    ]
+  }))
+  .pipe(gulp.dest('dist/img'))
+  .pipe($.size({
+    title: 'img',
+    showFiles: true
+  }))
+);
 
-gulp.task('img:clean', done => {
-  del(['dist/img'], done);
-});
+gulp.task('img:clean', () => del(['dist/img']));
 
-gulp.task('styleguide', () => {
-  return gulp.src('config.yml')
-    .pipe($.plumber())
-    .pipe($.hologram());
-});
+gulp.task('styleguide', () => gulp.src('config.yml')
+  .pipe($.plumber())
+  .pipe($.hologram())
+);
 
-gulp.task('html', ['styleguide'], () => {
-  return gulp.src('dist/**/*.html')
-    .pipe($.plumber())
-    .pipe($.minifyHtml())
-    .pipe(gulp.dest('dist'))
-    .pipe($.livereload())
-    .pipe($.size({
-      title: 'html',
-      showFiles: true
-    }));
-});
+gulp.task('html', ['styleguide'], () => gulp.src('dist/**/*.html')
+  .pipe($.plumber())
+  .pipe($.minifyHtml())
+  .pipe(gulp.dest('dist'))
+  .pipe($.livereload())
+  .pipe($.size({
+    title: 'html',
+    showFiles: true
+  }))
+);
 
-gulp.task('copy', () => {
-  return gulp.src([
-    'favicon.ico',
-    'CNAME',
-    'lib/**',
-    'audio/*'
-  ], {
-    base: '.'
-  })
-    .pipe($.plumber())
-    .pipe($.changed('dist'))
-    .pipe($.if('*.css', $.minifyCss({
-      keepSpecialComments: false
-    })))
-    .pipe($.if('*.js', $.uglify({
-      preserveComments: false
-    })))
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('copy', () => gulp.src([
+  'favicon.ico',
+  'CNAME',
+  'lib/**',
+  'audio/*'
+], {
+  base: '.'
+})
+  .pipe($.plumber())
+  .pipe($.changed('dist'))
+  .pipe($.if('*.css', $.minifyCss({
+    keepSpecialComments: false
+  })))
+  .pipe($.if('*.js', $.uglify({
+    preserveComments: false
+  })))
+  .pipe(gulp.dest('dist'))
+);
 
 gulp.task('build', [
   'css',
@@ -143,28 +130,24 @@ gulp.task('watch', ['build'], () => {
   gulp.watch('tpl/**/*.html', ['html']);
 });
 
-gulp.task('serve', ['watch'], () => {
-  return gulp.src('dist')
-    .pipe($.plumber())
-    .pipe($.webserver({
-      open: true,
-      livereload: true
-    }));
-});
+gulp.task('serve', ['watch'], () => gulp.src('dist')
+  .pipe($.plumber())
+  .pipe($.webserver({
+    open: true,
+    livereload: true
+  }))
+);
 
-gulp.task('deploy', ['deploy:clean', 'build'], () => {
-  return gulp.src('dist/**/*')
-    .pipe($.plumber())
-    .pipe($.ghPages({
-      branch: 'gh-pages',
-      cacheDir: '.tmp',
-      force: true
-    }))
-    .pipe($.size({
-      title: 'deploy'
-    }));
-});
+gulp.task('deploy', ['deploy:clean', 'build'], () => gulp.src('dist/**/*')
+  .pipe($.plumber())
+  .pipe($.ghPages({
+    branch: 'gh-pages',
+    cacheDir: '.tmp',
+    force: true
+  }))
+  .pipe($.size({
+    title: 'deploy'
+  }))
+);
 
-gulp.task('deploy:clean', done => {
-  del(['.tmp'], done);
-});
+gulp.task('deploy:clean', () => del(['.tmp']));
